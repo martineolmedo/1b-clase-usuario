ff6900728cbe9ab031722a9280ce02d739d16c1a
Martin Olmedo <martinolmedo078@gmail.com>
2025-10-05 13:08:05 -0300

    Agregadas la V2 y V3 del requisito

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 import { Usuario } from "./classes/Usuario.js";
 
-let usuario = Usuario.crear("Martin","adas@asdas.com",27,233);
+const btn = document.getElementById('btn')
 
-console.log(usuario)
+
+let usuario = Usuario.crear("Martin", "adas@asdas.com", 27);
+
+btn.addEventListener('click', () => {
+    const oldPin = document.getElementById('oldPin').value
+    const newPin = document.getElementById('newPin').value
+    const confirmPin = document.getElementById('confirmPin').value
+
+    if(newPin == confirmPin){
+        if(usuario.cambiarPin(oldPin,newPin)){
+            alert("Pin cambiado con exito!")
+        } else {
+            alert("Pin actual incorrecto, no se pudo cambiar")
+        }
+    } else {
+        alert('Los pines nuevos no coinciden, asegurate de escribirlos correctamente para que coincidan')
+    }
+})
--- classes/Usuario.js
+++ classes/Usuario.js
@@ -2,16 +2,24 @@ export class Usuario {
   nombre;
   email;
   edad;
-  #pin;
+  #pin = 1234;
 
-  constructor(nombre, email, edad, pin) {
+  constructor(nombre, email, edad) {
     this.nombre = nombre;
     this.email = email;
     this.edad = edad;
-    this.#pin = pin;
   }
 
-  static crear(nombre,email,edad,pin){
-    return new this(nombre,email,edad,pin)
+  static crear(nombre,email,edad){
+    return new this(nombre,email,edad)
+  }
+
+  cambiarPin(actual,nuevo){
+    if(actual == this.#pin){
+      this.#pin = nuevo
+      return true
+    } else {
+      return false
+    }
   }
 }
